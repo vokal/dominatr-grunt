7b6ed62c635defad73fe9e0d55711961ca731a02
Jarrett Widman <jarrett@24m2.com>
2015-07-02 16:46:37 -0500

    don't include components, changes on every project

--- grunt/concat.js
+++ grunt/concat.js
@@ -32,17 +32,6 @@ module.exports = function ( grunt )
                 "source/components/angular-mocks/angular-mocks.js"
             ],
             dest: "build/angular.js"
-        },
-        components:
-        {
-            src: [
-                "source/components/humps/humps.js",
-                "source/components/vokal-ng-api/dist/vokal-ng-api.min.js",
-                "source/components/route-auth/dist/route-auth.min.js",
-                "source/components/angular-local-storage/dist/angular-local-storage.min.js",
-                "source/components/html5shiv/dist/html5shiv.min.js"
-            ],
-            dest: "build/components.js"
         }
     };
 
