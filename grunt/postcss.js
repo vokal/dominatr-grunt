78c5be8ad82dd33abe88045e6e4da44dd6830bdc
Chris Dadabo <chris.dadabo@vokal.io>
2015-07-14 15:49:32 -0500

    Make browser support more betterer

--- grunt/postcss.js
+++ grunt/postcss.js
@@ -1,7 +1,7 @@
 module.exports = {
     options: {
         processors: [
-            require( "autoprefixer-core" )( { browsers: "last 2 versions" } ),
+            require( "autoprefixer-core" )( { browsers: "IE >= 9, > 1%" } ),
         ]
     },
     build: {
