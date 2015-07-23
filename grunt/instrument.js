06f915391977726aa172c22e2016119389589c92
Cory Faller <cory.faller@vokalinteractive.com>
2015-07-23 15:46:55 -0500

    Fix pattern

--- grunt/instrument.js
+++ grunt/instrument.js
@@ -1,5 +1,5 @@
 module.exports = {
-    files: [ "build/modules/**/*.js", "!build/modules/**/mocks/*.js", "!build/project.js" ],
+    files: [ "build/modules/**/*.js", "!build/modules/**/mocks/**/*.js", "!build/project.js" ],
     options: {
         lazy: false,
         basePath: "."
