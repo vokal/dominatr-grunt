4770a373842a5bf17517a98320ddf4f9ba948ed9
Cory Faller <cory.faller@vokalinteractive.com>
2015-08-13 13:18:25 -0500

    Streamline mocks checks

--- grunt/concat.js
+++ grunt/concat.js
@@ -12,11 +12,7 @@ module.exports = function ( grunt )
             ],
             filter: function ( filepath )
             {
-                if( /mocks/.test( filepath ) )
-                {
-                    return grunt.option( "mocks" );
-                }
-                return true;
+                return /mocks/.test( filepath ) ? grunt.option( "mocks" ) : true;
             },
             dest: "build/project.js"
         },
@@ -33,11 +29,7 @@ module.exports = function ( grunt )
             ],
             filter: function ( filepath )
             {
-                if( /mocks/.test( filepath ) )
-                {
-                    return grunt.option( "mocks" );
-                }
-                return true;
+                return /mocks/.test( filepath ) ? grunt.option( "mocks" ) : true;
             },
             dest: "build/angular.js"
         },
