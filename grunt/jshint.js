5a5641890d144ca2587c6a62569605dbe1f9fbd8
Cory Faller <cory.faller@vokalinteractive.com>
2015-08-14 15:48:09 -0500

    ...but only in dev

--- grunt/jshint.js
+++ grunt/jshint.js
@@ -1,10 +1,25 @@
 module.exports = {
     // See http://jshint.com/docs/options/ for all options
     // Options set to `false` are the jshintrc defaults
-    all:
+    dev:
     {
         options: {
             force: true,
+            jshintrc: ".jshintrc",
+        },
+        src:
+        [
+            "source/modules/*/*.js",
+            "source/modules/*/*/*.js",
+            "!source/modules/*/tests/**/*.*",
+            "grunt/*.js",
+            "tests/*.js"
+        ]
+    },
+    deploy:
+    {
+        options: {
+            force: false,
             jshintrc: ".jshintrc",
         },
         src:
