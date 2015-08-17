d7288527c74e823e82f8e38a2020a592113ab046
Cory Faller <cory.faller@vokalinteractive.com>
2015-08-17 11:06:13 -0500

    Define array once

--- grunt/jshint.js
+++ grunt/jshint.js
@@ -1,34 +1,32 @@
-module.exports = {
-    // See http://jshint.com/docs/options/ for all options
-    // Options set to `false` are the jshintrc defaults
-    dev:
-    {
-        options: {
-            force: true,
-            jshintrc: ".jshintrc"
+module.exports = function ()
+{
+    "use strict";
+
+    var src = [
+        "source/modules/*/*.js",
+        "source/modules/*/*/*.js",
+        "!source/modules/*/tests/**/*.*",
+        "grunt/*.js",
+        "tests/*.js"
+    ];
+
+    return {
+        dev:
+        {
+            options: {
+                force: true,
+                jshintrc: ".jshintrc"
+            },
+            src: src
         },
-        src:
-        [
-            "source/modules/*/*.js",
-            "source/modules/*/*/*.js",
-            "!source/modules/*/tests/**/*.*",
-            "grunt/*.js",
-            "tests/*.js"
-        ]
-    },
-    deploy:
-    {
-        options: {
-            force: false,
-            jshintrc: ".jshintrc"
-        },
-        src:
-        [
-            "source/modules/*/*.js",
-            "source/modules/*/*/*.js",
-            "!source/modules/*/tests/**/*.*",
-            "grunt/*.js",
-            "tests/*.js"
-        ]
-    }
+        deploy:
+        {
+            options: {
+                force: false,
+                jshintrc: ".jshintrc"
+            },
+            src: src
+        }
+    };
+
 };
