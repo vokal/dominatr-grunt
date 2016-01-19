cb5e94111d629769a9a07a6d323266a98c84bd5a
Devon Maloney <devon.maloney@vokal.io>
2016-01-19 10:11:14 -0600

    Remove trailing slash

--- grunt/cloudfront.js
+++ grunt/cloudfront.js
@@ -11,7 +11,7 @@ module.exports = function ( grunt )
         },
         build: {
             options: {
-                originPath: "/build-" + grunt.config( "version" ) + "/"
+                originPath: "/build-" + grunt.config( "version" )
             }
         }
     };
